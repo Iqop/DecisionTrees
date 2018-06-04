b47d7cb3ecb4a1b3ef046fbc4546c78e639262e4
Simão Silva <37107350+SFOS-SFOS@users.noreply.github.com>
2018-06-04 16:56:57 +0100

    Add files via upload

--- src/parser/ParserLine.java
+++ src/parser/ParserLine.java
@@ -1,40 +1,41 @@
 package parser;
+
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ParserLine implements Serializable {
 
-	private LinkedList<String> data;
+    private LinkedList<String> data;
 
-	ParserLine(String data){
-		Scanner dataScan = new Scanner(data);
-		this.data =new LinkedList<>();
-		dataScan.useDelimiter(",");
-		while(dataScan.hasNext()){
-			this.data.addLast(dataScan.next());
-		}
+    ParserLine(String data) {
+        Scanner dataScan = new Scanner(data);
+        this.data = new LinkedList<>();
+        dataScan.useDelimiter(",");
+        while (dataScan.hasNext()) {
+            this.data.addLast(dataScan.next());
+        }
 
-		dataScan.close();
-	}
+        dataScan.close();
+    }
 
-	public String get(int i){
-		return data.get(i);
-	}
+    public String get(int i) {
+        return data.get(i);
+    }
 
-	public void set(int i,String value){
-		data.set(i,value);
-	}
+    public void set(int i, String value) {
+        data.set(i, value);
+    }
 
-	public LinkedList<String> getAll(){
-		return data;
-	}
+    public LinkedList<String> getAll() {
+        return data;
+    }
 
-	public String toString(){
-		return data.toString()+"\n";
-	}
+    public String toString() {
+        return data.toString() + "\n";
+    }
 
-	public int size(){
-		return data.size();
-	}
+    public int size() {
+        return data.size();
+    }
 }
